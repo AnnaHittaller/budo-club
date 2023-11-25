451a62ebd6475cd5adee4a85cc520b86a55b9acb
Anna Hittaller <hittaller.anna@gmail.com>
2023-11-25 15:50:42 +0100

    axios added for form submission

--- src/views/KontaktView.vue
+++ src/views/KontaktView.vue
@@ -1,44 +1,62 @@
 <script setup>
-const handleSubmit = () => {
+import axios from 'axios'
+import { ref } from 'vue';
+let nachricht = {
+    name: '',
+    email: '',
+    text: '',
+}
+const toast = ref(null)
+
+const handleSubmit = async () => {
     console.log("submitted")
     // import axios, send form through it, add a reactive toast and dislay it with v-if
+    try {
+        const response = await axios.post("https://formspree.io/f/mleykoqo", nachricht)
+        console.log(nachricht, response)
+        toast.value = "Danke, deine Nachricht wurde gesendet!"
+
+        nachricht = {
+            name: '',
+            email: '',
+            text: '',
+        }
+
+    } catch (error) {
+        console.log('Error submitting form:', error.message)
+        toast.value = error.message
+    } finally {
+        // Ensure the toast is reset after a certain time
+        setTimeout(() => {
+            toast.value = null;
+        }, 3000);
+    }
 }
 </script>
 
 <template>
     <section class="bg-white text-gray-950">
-
         <div class="flex flex-col justify-center items-center">
             <h2>Kontakt</h2>
-            <p class="text-center max-w-[800px]">Du möchtest Dich zu einem kostenlosen und unverbindlichen Probetraining anmelden oder
-                hast noch offene
-                Fragen?
-                <br>
-                Dann sende gern eine Nachricht hier oder eine E-Mail an: trainer(at)shogun-euskirchen.de
-                oder melde Dich telefonisch unter: 0172 / 2627062.
-                <br>
-                Bitte verwende nicht die Adresse unseres Dojos um Kontakt zu uns aufzunehmen.
-            </p>
-            <form id="contact-form" name="simple-contact-form" action="https://formspree.io/f/mleykoqo" method="post" @submit.prevent="handleSubmit"
-                class="w-full mt-8 max-w-xl">
+            <!-- ... -->
+            <form id="contact-form" name="simple-contact-form" action="https://formspree.io/f/mleykoqo" method="post"
+                @submit.prevent="handleSubmit" class="w-full mt-8 max-w-xl">
                 <fieldset class="flex flex-col justify-center w-full m-auto gap-4">
                     <label for="name">Name
-
-                        <input type="text" name="name" id="name" placeholder="Deine Name" required="true">
+                        <input type="text" name="name" id="name" placeholder="Deine Name" required v-model="nachricht.name">
                     </label>
                     <label for="email-address">E-Mail Addresse
-
-                        <input type="email" name="_replyto" id="email-address" placeholder="email@domain.de"
-                            required="true">
+                        <input type="email" name="_replyto" id="email-address" placeholder="email@domain.de" required
+                            v-model="nachricht.email">
                     </label>
                     <label for="message">Nachricht
-
-                        <textarea rows="5" name="message" id="message" placeholder="Deine Nachricht"
-                            required="true"></textarea>
+                        <textarea rows="5" name="message" id="message" placeholder="Deine Nachricht" required
+                            v-model="nachricht.text"></textarea>
                     </label>
                     <input type="hidden" name="_subject" id="email-subject"
                         value="Nachricht durch der Kontaktformular der Webseite">
                 </fieldset>
+                <p class="w-full text-red-500 font-bold mt-4" v-if="toast !== null">{{ toast }}</p>
                 <button type="submit" class="btn max-md:ml-[50%] max-md:translate-x-[-50%]">Nachricht senden</button>
             </form>
         </div>
@@ -58,7 +76,8 @@ textarea {
     outline: none;
 }
 
-input:focus, textarea:focus {
+input:focus,
+textarea:focus {
     border-color: rgb(239 68 68);
 }
 
